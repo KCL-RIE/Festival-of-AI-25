a79c95d1e3227f5d7610e11076e5c0f181bbf9df
Hayeong Lee <hayeong.lee@kcl.ac.uk>
2025-02-17 01:22:57 +0000

    Add socre board

--- ui/pages/tv/game.vue
+++ ui/pages/tv/game.vue
@@ -6,9 +6,21 @@
         <div class="container-1">
             <div class="game-info">
                 <div class="score">
-                    <div class="title">
-                        <h1>score</h1>
+                    <div class=container-info>
+                        <div class="title">
+                            <h1>score</h1>
+                        </div>
+                        <div class="board">
+                            <div class="team-rie"></div>
+                            <div class="number">
+                                <span>{{ rie_score }}</span>
+                                <span> : </span>
+                                <span>{{ human_score }}</span>
+                            </div>
+                            <div class="team-human"></div>
+                        </div>
                     </div>
+                    
                 </div>
 
                 <div class="panel">
@@ -78,12 +90,54 @@
     text-align: center;
 }
 
+.container-info {
+    display: flex;
+    flex-direction: column;
+}
+
 .title {
     position: absolute;
     top: 20%;
     left: 50%;
     transform: translate(-50%, -50%);
     font-size: clamp(10px, 4vw, 48px);
+}
+
+.board {
+    display: flex;
+    flex-grow: 4;
+    flex-direction: row;
+}
+
+.team-rie {
+    background-image: url('/team-rie.png');
+    background-size: 100% 100%;
+    background-position: bottom;
+    position: absolute;
+    width: 100px;
+    height: 100px;
+    top: 60%;
+    left: 20%;
+    transform: translate(-50%, -50%);
+}
+
+.number {
+    position: absolute;
+    top: 60%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+
+.team-human {
+    background-image: url('/team-rie.png');
+    background-size: 100% 100%;
+    background-position: bottom;
+    position: absolute;
+    width: 100px;
+    height: 100px;
+    top: 60%;
+    left: 80%;
+    transform: translate(-50%, -50%);
 }
 
 .panel {
